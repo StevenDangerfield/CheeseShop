d322309d7ecc19b4e9880372da24c0fd009a0795
Steven Dangerfield <steven.dangerfield@workingmouse.com.au>
2024-07-15 20:24:14 +1000

    Basic cheese page and list

---
+++ CheeseShopWeb/src/main/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+import CheeseList from './CheeseList'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <CheeseList/>
+    </>
+  )
+}
+
+export default App
---
+++ CheeseShopWeb/src/main/CheeseList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CheeseList() {
+    return (
+        <>
+            <h2>Cheese List</h2>
+            <ul>Cheese 1</ul>
+            <ul>Cheese 2</ul>
+            <ul>Cheese 3</ul>
+            <ul>Cheese 4</ul>
+            <ul>Cheese 5</ul>
+        </>
+    );
+}
+
+export default CheeseList;
